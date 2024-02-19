799ead8ff3026ee6b231e1ae56c2a627c269e078
imylomylo <mylo@cakeshop.dev>
2024-02-19 21:22:38 +0200

    (feature) add initial verus rpc calls

--- src/views/Currencies.vue
+++ src/views/Currencies.vue
@@ -1,12 +1,28 @@
 <script setup>
+console.log("currencies setup")
+// import currencyHelpers from '../lib/currencyHelpers';
 
+</script>
+<script>
+import { ref } from 'vue'
+import currencyHelpers from '../lib/currencyHelpers';
+let info = ref([])
+let currencies = ref([])
+info = await currencyHelpers.getInfo()
+currencies = await currencyHelpers.listCurrencies()
+export default {
+  name: "Verus Currencies"
+}
 </script>
 
 <template>
-  <div class="container">
-    <h1>Currencies</h1>
-  </div>
+    <div class="container">
+      <h1>Currencies</h1>
+      {{ info.VRSCversion }}
+      <li v-for="(currency, index) in currencies">
+        {{ currency.currencydefinition.fullyqualifiedname }}
+      </li>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
