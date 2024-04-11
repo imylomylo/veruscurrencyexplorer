5c3614bdc14ddd959d69c7fc4aead4e82759075c
iamahmedshahh <artistahmed41@gmail.com>
2024-04-11 16:47:49 +0500

    daisy ui import successful

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes : false,
-    darkTheme: 'dark',
-  },
+  plugins: [require("daisyui")],
 }
-
