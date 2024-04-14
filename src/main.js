04b3207637f2f429fd6621959986597e6153f7de
iamahmedshahh <artistahmed41@gmail.com>
2024-04-15 01:52:53 +0500

    removing vuetify completely to remove redundant network req

--- src/main.js
+++ src/main.js
@@ -2,19 +2,10 @@ import { createApp } from 'vue'
 import './index.css'
 import App from './App.vue'
 import router from './router'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 import store from './store'; // Import your Vuex store
 import '@mdi/font/css/materialdesignicons.css';
 
 
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
-  
   createApp(App)
   .use(router)
   .use(store)
